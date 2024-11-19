a4043bb08d71a908ffeb11ddd70077614712a070
Darus Salam <darussalam66d@gmail.com>
2024-11-19 14:47:40 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/ay_bay_app/ay_bay_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ay_bay_app.ay_bay_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AyBayHishab());
+}
+class AyBayHishab extends StatelessWidget {
+  const AyBayHishab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ay_Bay_Hishab"),
+      ),
+    );
+  }
+}
+
