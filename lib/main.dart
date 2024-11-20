9dab9c3f6e1be53d5563d37bdf9f46e1b760e331
Darus Salam <darussalam66d@gmail.com>
2024-11-20 13:48:30 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ay_bay_app/categorywidget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -26,31 +27,80 @@ class AyBayHishab extends StatelessWidget {
     );
   }
 }
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final ayTEController = TextEditingController();
+  final bayTEController = TextEditingController();
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Ay_Bay_Hishab"),
+        title: Text("Ay Bay Hishab"),
         centerTitle: true,
         backgroundColor: Colors.deepPurple.shade200,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(child: TextField()),
-                SizedBox(width: 8,),
-                Expanded(child: TextField()),
-              ],
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Form(
+                key: formKey,
+                child: Row(
+                  children: [
+                    Expanded(child: TextFormField(
+                      controller: ayTEController,
+                      decoration: InputDecoration(
+                        labelText: "Ay",
+                        isDense: true,
+                      ),
+                    )),
+                    SizedBox(width: 8,),
+                    Expanded(child: TextFormField(
+                      controller: bayTEController,
+                      decoration: InputDecoration(
+                        labelText: "Bay",
+                        isDense: true,
+                      ),
+                    )),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10,),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurple,
+                      foregroundColor: Colors.white,
+                    ),
+                      onPressed: (){}, child: Text("Add"))),
+              const SizedBox(height: 10,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CategoryWidget(title: 'Ay', color: Colors.green,),
+                  CategoryWidget(title: 'Date', color: Colors.blue,),
+                  CategoryWidget(title: 'Bay', color: Colors.deepOrangeAccent,),
+                  CategoryWidget(title: 'More', color: Colors.brown,),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+
 
